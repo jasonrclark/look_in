5672de76ef5b98badd979f0e9e45307540f5c240
Jason R. Clark <jclark@newrelic.com>
2015-05-01 16:47:12 -0700

    Basic functionality for const lookups

--- lib/look_in.rb
+++ lib/look_in.rb
@@ -1,5 +1,20 @@
 require "look_in/version"
 
-module LookIn
-  # Your code goes here...
+Module.module_eval do
+  def look_in(*constants)
+    @look_in ||= []
+    @look_in.concat(constants)
+
+    if !self.respond_to?(:const_missing_without_look_in)
+      class << self
+        alias_method :const_missing_without_look_in, :const_missing
+
+        def const_missing(name)
+          found_in = @look_in.find { |look| look.const_defined?(name) }
+          return found_in.const_get(name) if found_in
+          return const_missing_without_look_in(name)
+        end
+      end
+    end
+  end
 end
--- test/look_in_test.rb
+++ test/look_in_test.rb
@@ -1,7 +1,41 @@
 require 'minitest_helper'
 
 class TestLookIn < Minitest::Test
+  module Outer
+    module Inner
+      module Innermost
+      end
+    end
+  end
+
+  module Home
+    module Room
+      look_in Outer::Inner
+
+      def self.door
+        Innermost
+      end
+    end
+
+    module Shed
+      look_in Outer
+
+      def self.floor
+        Inner::Innermost
+      end
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::LookIn::VERSION
   end
+
+  def test_look_in
+    assert_equal Outer::Inner::Innermost, Home::Room.door
+  end
+
+  def test_finds_nested_consts
+    assert_equal Outer::Inner::Innermost, Home::Shed.floor
+  end
+
 end
